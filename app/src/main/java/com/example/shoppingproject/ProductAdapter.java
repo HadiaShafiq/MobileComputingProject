e0975eb7044ce0ba0cf043ccc9559705b0a7840a
Saniah22 <60928097+Saniah22@users.noreply.github.com>
2020-07-13 01:26:18 +0500

    Add files via upload

---
+++ app/src/main/java/com/example/shoppingproject/ProductAdapter.java
@@ -0,0 +1,68 @@
+package com.example.shoppingproject;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>  {
+
+    private List<Product> data = new ArrayList<Product>();
+    private LayoutInflater inflater;
+    Context context;
+
+    public ProductAdapter(Context context, List<Product> data){
+        this.context=context;
+        inflater= LayoutInflater.from(context);
+        this.data=data;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view=inflater.inflate(R.layout.product_lists, parent,false);
+        ProductAdapter.ProductHolder holder=new ProductAdapter.ProductHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+// Get current position of item in recyclerview to bind data and assign values from list
+        final ProductAdapter.ProductHolder myHolder= (ProductAdapter.ProductHolder) holder;
+        final Product product=data.get(position);
+        myHolder.txtTitle.setText(product.getproductName());
+
+        //convert byte to bitmap take from Category class
+        byte[] outImage=product.getproductImg();
+        ByteArrayInputStream imageStream = new ByteArrayInputStream(outImage);
+        Bitmap theImage = BitmapFactory.decodeStream(imageStream);
+        myHolder.imgIcon.setImageBitmap(theImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+    public class ProductHolder extends RecyclerView.ViewHolder{
+        // init the item view's
+        TextView txtTitle;
+        ImageView imgIcon;
+        public ProductHolder(View itemView) {
+            super(itemView);
+            // get the reference of item view's
+            txtTitle= (TextView) itemView.findViewById(R.id.producttxtTitle);
+            imgIcon= (ImageView) itemView.findViewById(R.id.productImgIcon);
+        }
+    }
+}
